497372195e7a7e19f4c969f489ccf263fbc3ddae
Rahul Ghosal <rdghosal@gmail.com>
2024-02-23 08:51:12 -0500

    feat: initial ci-image

--- ci-image/src/main.rs
+++ ci-image/src/main.rs
@@ -1,3 +1,20 @@
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+    let output = Command::new("uv").args(["pip", "freeze"]).output();
+    let mut package_names = Vec::<&str>::new();
+    if let Ok(o) = output {
+        let requirements =
+            String::from_utf8(o.stdout).expect("Failed to convert stdout from `uv pip freeze`");
+        for requirement in requirements.split("\n") {
+            let package_name = requirement.split("==").nth(0).unwrap();
+            if package_name.is_empty() {
+                continue;
+            }
+            package_names.push(package_name);
+        }
+        dbg!(package_names);
+    } else {
+        panic!("oops!");
+    }
 }
