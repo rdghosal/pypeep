dc3ba89fe69028829fa9dd79bbf784a8bfefe0f0
Rahul Ghosal <rdghosal@gmail.com>
2024-02-25 17:31:36 -0500

    feat: add mode tables with proper modeling

--- scripts/init_sql.py
+++ scripts/init_sql.py
@@ -1,15 +1,59 @@
-"""Setup script to initalize database table(s) with dummy data."""
+"""Creates tables in database."""
 import sqlite3
 
 
-con = sqlite3.connect('pypeep.db')
-cur = con.cursor()
-cur.execute('CREATE TABLE requirements(id, name, current_version)')
+DB_PATH = 'pypeep.db'
 
-params = list()
-for i, dep in enumerate(('flask', 'pydantic', 'pandas')):
-    params.append((i + 1, dep, None))
-cur.executemany('INSERT INTO requirements VALUES(?, ?, ?)', params)
+DROP_PROJECTS = "DROP TABLE IF EXISTS projects;"
+CREATE_PROJECTS = """
+    CREATE TABLE projects (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP
+        );
+    """
+DROP_REQUIREMENTS = "DROP TABLE IF EXISTS requirements;"
+CREATE_REQUIREMENTS = """
+    CREATE TABLE requirements (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        current_version VARCHAR(10) NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP
+        );
+    """
+DROP_PROJECT_REQUIREMENTS = "DROP TABLE IF EXISTS project_requirements;"
+CREATE_PROJECT_REQUIREMENTS = """
+    CREATE TABLE project_requirements (
+        project_name TEXT NOT NULL,
+        requirement TEXT NOT NULL,
+        current_version VARCHAR(10) NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP,
+        PRIMARY KEY (project_name, requirement),
+        FOREIGN KEY(project_name) REFERENCES projects(name),
+        FOREIGN KEY(requirement) REFERENCES requirements(name)
+        );
+    """
+SCRIPTS = (
+    DROP_PROJECTS,
+    CREATE_PROJECTS,
+    DROP_REQUIREMENTS,
+    CREATE_REQUIREMENTS,
+    DROP_PROJECT_REQUIREMENTS,
+    CREATE_PROJECT_REQUIREMENTS,
+)
+
+
+con = sqlite3.connect(DB_PATH)
+cur = con.cursor()
+
+for i, script in enumerate(SCRIPTS):
+    print(f"running script {i + 1} of {len(SCRIPTS)}")
+    cur.execute(script)
+    con.commit()
+
 con.commit()
 cur.close()
 con.close()
